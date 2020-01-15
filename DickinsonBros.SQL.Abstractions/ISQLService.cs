bc2ef00658f9eebd390e219fbf6d641dbb23a801
Mark Dickinson <marksamdickinson@gmail.com>
2020-01-15 11:28:36 -0700

    Add project files.

---
+++ DickinsonBros.SQL.Abstractions/ISQLService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DickinsonBros.SQL.Abstractions
+{
+    public interface ISQLService
+    {
+        Task ExecuteAsync(string connectionString, string sql, object param = null, CommandType? commandType = null);
+        Task<T> QueryFirstAsync<T>(string connectionString, string sql, object param = null, CommandType? commandType = null);
+        Task<T> QueryFirstOrDefaultAsync<T>(string connectionString, string sql, object param = null, CommandType? commandType = null);
+    }
+}
