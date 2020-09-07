6d06c6b40f3648ba2c8fa2b22f970601ee26a8d6
Mark Dickinson <marksamdickinson@gmail.com>
2020-09-06 22:00:36 -0700

    Update Interface

--- DickinsonBros.SQL.Abstractions/ISQLService.cs
+++ DickinsonBros.SQL.Abstractions/ISQLService.cs
@@ -11,7 +11,8 @@ namespace DickinsonBros.SQL.Abstractions
         Task ExecuteAsync(string connectionString, string sql, object param = null, CommandType? commandType = null);
         Task<T> QueryFirstAsync<T>(string connectionString, string sql, object param = null, CommandType? commandType = null);
         Task<T> QueryFirstOrDefaultAsync<T>(string connectionString, string sql, object param = null, CommandType? commandType = null);
-        Task BulkCopyAsync<T>(string connectionString, DataTable table, string tableName, int? batchSize, TimeSpan? timeout, CancellationToken? token);
+        Task BulkCopyAsync(string connectionString, DataTable table, string tableName, int? batchSize, TimeSpan? timeout, CancellationToken? token);
+        Task BulkCopyAsync<T>(string connectionString, IEnumerable<T> enumerable, string tableName, int? batchSize, TimeSpan? timeout, CancellationToken? token);
         Task<IEnumerable<T>> QueryAsync<T>(string connectionString, string sql, object param = null, CommandType? commandType = null);
     }
 }
